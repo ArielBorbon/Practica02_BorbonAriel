c70229c1265dff66bb75a74f680fb1f342ac2e2e
ArielBorbon <ariel.borbon252116@potros.itson.edu.mx>
2026-02-19 04:18:54 -0700

    login Funcional
    Con que ningun campo este vacio permite acceder a la pelicula con el boton

--- AmanteDeLasPeliculas_252116/app/src/main/java/borbon/ariel/amantedelaspeliculas_252116/login.kt
+++ AmanteDeLasPeliculas_252116/app/src/main/java/borbon/ariel/amantedelaspeliculas_252116/login.kt
@@ -1,6 +1,9 @@
 package borbon.ariel.amantedelaspeliculas_252116
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,35 @@ class login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val botonLogin = findViewById<Button>(R.id.btnLogin)
+        val textoUsuario  = findViewById<EditText>(R.id.txtNombre)
+        val textoContrasena = findViewById<EditText>(R.id.txtContrasena)
+
+
+
+
+
+        botonLogin.setOnClickListener {
+
+            val intentPelicula = Intent(this , MainActivity::class.java)
+
+
+            val textoObtenidoUsuario = textoUsuario.text.toString()
+            val textoObtenidoContrasena = textoContrasena.text.toString()
+
+            if (textoObtenidoContrasena.isEmpty() || textoObtenidoUsuario.isEmpty()){
+                textoUsuario.setText("")
+                textoContrasena.setText("")
+                return@setOnClickListener
+            }
+
+            startActivity(intentPelicula)
+
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
